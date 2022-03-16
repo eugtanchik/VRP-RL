8d36387d0691fbe34c58cf42acdea6bdfd97488f
Eugene Tanchik <yevhen.tanchik@cognite.com>
2022-03-16 19:24:25 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import numpy as np
-from tqdm import tqdm 
+from tqdm import tqdm
 import tensorflow as tf
 import time
 
@@ -10,21 +10,21 @@ from configs import ParseParams
 from shared.decode_step import RNNDecodeStep
 from model.attention_agent import RLAgent
 
+
 def load_task_specific_components(task):
-    '''
+    """
     This function load task-specific libraries
-    '''
+    """
     if task == 'tsp':
-        from TSP.tsp_utils import DataGenerator, Env ,reward_func
+        from TSP.tsp_utils import DataGenerator, Env, reward_func
         from shared.attention import Attention
 
         AttentionActor = Attention
         AttentionCritic = Attention
 
-
     elif task == 'vrp':
-        from VRP.vrp_utils import DataGenerator,Env,reward_func
-        from VRP.vrp_attention import AttentionVRPActor,AttentionVRPCritic
+        from VRP.vrp_utils import DataGenerator, Env, reward_func
+        from VRP.vrp_attention import AttentionVRPActor, AttentionVRPCritic
 
         AttentionActor = AttentionVRPActor
         AttentionCritic = AttentionVRPCritic
@@ -32,8 +32,8 @@ def load_task_specific_components(task):
     else:
         raise Exception('Task is not implemented')
 
-
     return DataGenerator, Env, reward_func, AttentionActor, AttentionCritic
+
 
 def main(args, prt):
     config = tf.ConfigProto()
@@ -65,30 +65,29 @@ def main(args, prt):
         train_time_beg = time.time()
         for step in range(args['n_train']):
             summary = agent.run_train_step()
-            _, _ , actor_loss_val, critic_loss_val, actor_gra_and_var_val, critic_gra_and_var_val,\
-                R_val, v_val, logprobs_val,probs_val, actions_val, idxs_val= summary
+            _, _, actor_loss_val, critic_loss_val, actor_gra_and_var_val, critic_gra_and_var_val, \
+            R_val, v_val, logprobs_val, probs_val, actions_val, idxs_val = summary
 
-            if step%args['save_interval'] == 0:
-                agent.saver.save(sess,args['model_dir']+'/model.ckpt', global_step=step)
+            if step % args['save_interval'] == 0:
+                agent.saver.save(sess, args['model_dir'] + '/model.ckpt', global_step=step)
 
-            if step%args['log_interval'] == 0:
-                train_time_end = time.time()-train_time_beg
-                prt.print_out('Train Step: {} -- Time: {} -- Train reward: {} -- Value: {}'\
-                      .format(step,time.strftime("%H:%M:%S", time.gmtime(\
-                        train_time_end)),np.mean(R_val),np.mean(v_val)))
-                prt.print_out('    actor loss: {} -- critic loss: {}'\
-                      .format(np.mean(actor_loss_val),np.mean(critic_loss_val)))
+            if step % args['log_interval'] == 0:
+                train_time_end = time.time() - train_time_beg
+                prt.print_out('Train Step: {} -- Time: {} -- Train reward: {} -- Value: {}' \
+                              .format(step, time.strftime("%H:%M:%S", time.gmtime( \
+                    train_time_end)), np.mean(R_val), np.mean(v_val)))
+                prt.print_out('    actor loss: {} -- critic loss: {}' \
+                              .format(np.mean(actor_loss_val), np.mean(critic_loss_val)))
                 train_time_beg = time.time()
-            if step%args['test_interval'] == 0:
+            if step % args['test_interval'] == 0:
                 agent.inference(args['infer_type'])
 
-    else: # inference
+    else:  # inference
         prt.print_out('Evaluation started ...')
         agent.inference(args['infer_type'])
 
+    prt.print_out('Total time is {}'.format(time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))))
 
-    prt.print_out('Total time is {}'.format(\
-        time.strftime("%H:%M:%S", time.gmtime(time.time()-start_time))))
 
 if __name__ == "__main__":
     args, prt = ParseParams()
